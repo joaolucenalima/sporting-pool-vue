4f4e1497952c9858869ef1aa902bae617e142447
joaolucenalima <joaovitorlucena000@gmail.com>
2024-04-08 00:53:14 -0300

    feat: add create pool form

--- src/components/CreatePoolModal.vue
+++ src/components/CreatePoolModal.vue
@@ -2,6 +2,7 @@
 import ModalComponent from '@/components/ModalComponent.vue';
 import PrimaryButton from '@/components/PrimaryButton.vue';
 import { ref } from 'vue';
+import InputText from './InputText.vue';
 
 const isModalOpened = ref(false)
 
@@ -21,8 +22,49 @@ const closeModal = () => {
     <template #title>
       <span>Crie um novo bolão</span>
     </template>
-    <template #content>
-      <p>Insira o nome do bolão e o valor da aposta</p>
+    <template #form>
+      <div class="form-content">
+        <div class="form-field">
+          <label>Nome do bolão</label>
+          <InputText placeholder="Ex.: Bolão da firma" />
+        </div>
+
+        <div class="form-field">
+          <label>Código do bolão</label>
+          <InputText placeholder="AAAA-BBBB-CCCC-DDDD" />
+        </div>
+
+        <div class="form-field">
+          <label>Máx. participantes</label>
+          <input type="number" name="participantes" value="10" min="2" max="30">
+        </div>
+
+        <PrimaryButton text="Criar" style="align-self: flex-end; width: 30%; margin-top: -1rem;" />
+      </div>
     </template>
   </ModalComponent>
 </template>
+
+<style scoped>
+.form-content {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+
+.form-content label {
+  font-weight: 400;
+  font-size: 30px;
+  display: block;
+}
+
+.form-content input[type="number"] {
+  max-width: 20%;
+  padding: 1rem;
+  font-size: 30px;
+  border: 1px solid #DDDDDD;
+  background-color: #f2f2f2;
+  border-radius: 8px;
+  text-align: center;
+}
+</style>
