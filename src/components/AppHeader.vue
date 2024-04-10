7022c5a6f62bc49210ae87880c237ddad67ec948
joaolucenalima <joaovitorlucena000@gmail.com>
2024-04-10 14:22:00 -0300

    change pools-page-layout div to PoolsView

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,31 +1,20 @@
 <script setup>
-import CreatePoolModal from './CreatePoolModal.vue';
 import ProjectLogo from './ProjectLogo.vue';
 import UserHello from './UserHello.vue';
 </script>
 
 <template>
-  <div class="pools-page-layout">
-    <header>
-      <ProjectLogo />
-      <nav>
-        <router-link to="/calendar">Calendar</router-link>
-        <router-link to="/pools">Pools</router-link>
-      </nav>
-      <UserHello />
-    </header>
-
-    <CreatePoolModal style="align-self: flex-end;" />
-  </div>
+  <header>
+    <ProjectLogo />
+    <nav>
+      <router-link to="/calendar">Calendar</router-link>
+      <router-link to="/pools">Pools</router-link>
+    </nav>
+    <UserHello />
+  </header>
 </template>
 
 <style scoped>
-.pools-page-layout {
-  display: flex;
-  flex-direction: column;
-  gap: 3rem;
-}
-
 header {
   display: grid;
   grid-template-columns: max-content auto max-content;
--- src/views/PoolsView.vue
+++ src/views/PoolsView.vue
@@ -1,7 +1,20 @@
 <script setup>
 import AppHeader from '@/components/AppHeader.vue';
+import CreatePoolModal from '@/components/CreatePoolModal.vue';
 </script>
 
 <template>
-  <AppHeader />
+  <div class="pools-page-layout">
+    <AppHeader />
+
+    <CreatePoolModal style="align-self: flex-end;" />
+  </div>
 </template>
+
+<style scoped>
+.pools-page-layout {
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+}
+</style>
